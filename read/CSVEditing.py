cb5a9c3fc554e0b975c55b29b9c4df0873141e26
sshivam95 <sshivam95@gmail.com>
2018-04-17 00:53:21 +0530

    First commit

---
+++ read/CSVEditing.py
@@ -0,0 +1,19 @@
+import csv
+import pandas as pd
+
+def process_file(file_handle):
+    df = pd.read_csv(file_handle)
+    Predicted_Category = []
+    # print(csvfile.columns)
+    for index, row in df.iterrows():
+        # print(row[4])
+        Predicted_Category.append("Unknown")
+
+    if 'Predicted_Category' not in df.columns:
+        df.insert(loc=0, column='Predicted_Category', value=Predicted_Category)
+    else:
+        print('column name already exists')
+        New_Column = pd.Series(Predicted_Category)
+        df['Predicted_Category'] = New_Column.values
+
+    return df
